15626a6891b5e0391a4eccb202fed35c77026e86
Chojnacki <twojemail@example.com>
2024-04-30 18:58:08 +0200

    .

---
+++ src/main/java/pl/javastart/matmovieclub/MatMovieClubApplication.java
@@ -0,0 +1,13 @@
+package pl.javastart.matmovieclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatMovieClubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatMovieClubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/javastart/matmovieclub/MatMovieClubApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.javastart.matmovieclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatMovieClubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
