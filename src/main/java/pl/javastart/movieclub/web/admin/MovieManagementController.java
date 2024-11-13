e3fe6561f816aaea82861d27948aad0f4842544d
MatChojnacki <mateus__z@wp.pl>
2024-11-13 21:45:57 +0100

    .

--- src/main/java/pl/javastart/movieclub/web/admin/MovieManagementController.java
+++ src/main/java/pl/javastart/movieclub/web/admin/MovieManagementController.java
@@ -4,8 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import pl.javastart.movieclub.domain.genre.GenreService;
 import pl.javastart.movieclub.domain.genre.dto.GenreDto;
